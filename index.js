e86e236239c6bbe68bb3e803a49124a2ba06fc15
Pruthvi <pruthvirajput97@gmail.com>
2020-10-10 21:32:10 +0530

    add image

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var numOfButton = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numOfButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("click");
+    this.style.color = "white";
   });
 }
